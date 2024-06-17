fecbc6a307f53faba0d391aa054275bbc3a4933b
byHYONS <byhyons@gmail.com>
2024-06-17 16:27:51 +0200

    [UPD] add form in index.php and paragraphe in censure.php

--- censure.php
+++ censure.php
@@ -1,3 +1,23 @@
 <?php
 
-$censure = $_GET["name"];
+$censure = $_GET["bedwords"];
+
+echo $censure . ' ' . strlen($censure);
+
+echo str_replace('nzo', '***', $censure);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>La parola è <?php echo $censure ?> la lunghezza è <?php echo strlen($censure) ?> </p>
+    <p>La parola è <?php echo str_replace('nzo', '***', $censure) ?> la lunghezza è <?php echo strlen($censure) ?> </p>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,3 +3,21 @@
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body>
+
+    <form action="censure.php" method="GET">
+        <label for="bedwords">Scrivi la bedwords</label>
+        <input type="text" id="bedwords" name="bedwords">
+        <button>Invia</button>
+    </form>
+    <p></p>
+    
+</body>
+</html>
